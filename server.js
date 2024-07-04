72fa2107cbbeb4230611a63d0eb0f1148b13c8dd
Samir Idris <samzidris11@gmail.com>
2024-07-04 21:11:42 +0100

    initial files

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
+const path = require('path');
+const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
+const PORT = process.env.port | 3500;
+
+// Custom middleware logger
+app.use(logger);
+
+// CORS - Cross Origin Resource Sharing
+app.use(cors(corsOptions));
+
+// Process form data
+app.use(express.urlencoded({ extended: false }));
+
+// Process json
+app.use(express.json());
+
+// Serve static files
+app.use('/', express.static(path.join(__dirname, '/public')));
+
+// Routes
+app.use('/', require('./routes/root'));
+app.use('/register', require('./routes/register'));
+app.use('/auth', require('./routes/auth'));
+app.use('/employees', require('./routes/api/employees'));
+
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ error: '404 not found' });
+    } else {
+        res.type('txt').send('404 not found');
+    }
+});
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
